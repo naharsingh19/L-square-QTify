510cfeffd2fad82e0c5662f3eddc0ffa99b3ce66
Nahar <nahartewatia19@gmail.com>
2023-11-06 14:47:00 +0530

    hero qtify takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import HeroImage from './Components/HeroImage/HeroImage';
 import  Navbar from './Components/Navbar/Navbar'
 function App() {
   return (
-    <Navbar/>
-  );
+    <div>
+     <Navbar />
+     <HeroImage />
+    </div>
+   );
 }
 
 export default App;
